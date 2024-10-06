81410f2469e78137c6beb16bf61e8fd98da93e47
dinesh <dineshbhagatbpl@gmail.com>
2024-10-06 17:24:26 +0530

    fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,20 +4,20 @@
 
 export const environment = {
   production: false,
-     
-    //apiUrl: 'http://localhost/search_product.php',
-    //apiUrl: 'https://tsoliteuat.tradesmartonline.in/api/v5_uat/public/',
-    //apiUrl: 'http://localhost/employee.php',
-    
-    //getUserDetailsApiURL: 'http://localhost/getUserLoginDetails.php',
-   // searchApiUrl: 'http://localhost/search_product.php',
-   //uploadDataApiUrl: 'http://localhost/userdatabase.php',
-   //insertUserDetailsApiUrl: 'http://localhost/isertUserDetailsData.php',
 
-    getUserDetailsApiURL: 'http://192.168.249.250/getUserLoginDetails.php',
-    searchApiUrl: 'http://192.168.249.250/search_product.php',
-    uploadDataApiUrl: 'http://192.168.249.250/userdatabase.php',
-    insertUserDetailsApiUrl: 'http://192.168.249.250/isertUserDetailsData.php',
+  //apiUrl: 'http://localhost/search_product.php',
+  //apiUrl: 'https://tsoliteuat.tradesmartonline.in/api/v5_uat/public/',
+  //apiUrl: 'http://localhost/employee.php',
+
+  //getUserDetailsApiURL: 'http://localhost/getUserLoginDetails.php',
+  // searchApiUrl: 'http://localhost/search_product.php',
+  //uploadDataApiUrl: 'http://localhost/userdatabase.php',
+  //insertUserDetailsApiUrl: 'http://localhost/insertUserDetailsData.php',
+
+  getUserDetailsApiURL: 'http://192.168.249.250/getUserLoginDetails.php',
+  searchApiUrl: 'http://192.168.249.250/search_product.php',
+  uploadDataApiUrl: 'http://192.168.249.250/userdatabase.php',
+  insertUserDetailsApiUrl: 'http://192.168.249.250/insertUserDetailsData.php',
 
 };
 
